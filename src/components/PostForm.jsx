221cce6489c0c299712401f713b8c655b8918d6c
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-10-10 09:50:32 +0200

    Update PostForm.jsx

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -8,7 +8,7 @@ export default function PostForm({ savePost, post }) {
     const [errorMessage, setErrorMessage] = useState("");
 
     useEffect(() => {
-        if (post) {
+        if (post.title && post.body && post.image) {
             // if post, set the states with values from the post object.
             // The post object is a prop, passed from UpdatePage
             setTitle(post.title);
@@ -60,16 +60,36 @@ export default function PostForm({ savePost, post }) {
         <form onSubmit={handleSubmit}>
             <label>
                 Title
-                <input type="text" value={title} placeholder="Type a title" onChange={e => setTitle(e.target.value)} />
+                <input
+                    type="text"
+                    value={title}
+                    placeholder="Type a title"
+                    onChange={e => setTitle(e.target.value)}
+                />
             </label>
             <label>
                 Body
-                <input type="text" value={body} placeholder="Type a body text" onChange={e => setBody(e.target.value)} />
+                <input
+                    type="text"
+                    value={body}
+                    placeholder="Type a body text"
+                    onChange={e => setBody(e.target.value)}
+                />
             </label>
             <label>
                 Image
-                <input type="file" className="file-input" accept="image/*" onChange={handleImageChange} />
-                <img className="image-preview" src={image} alt="Choose" onError={event => (event.target.src = imgPlaceholder)} />
+                <input
+                    type="file"
+                    className="file-input"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                />
+                <img
+                    className="image-preview"
+                    src={image}
+                    alt="Choose"
+                    onError={event => (event.target.src = imgPlaceholder)}
+                />
             </label>
             <p className="text-error">{errorMessage}</p>
             <button type="submit">Save</button>

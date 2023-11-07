e9728dc9cdabf9ef6a2c4171e9c348589f8d0dfe
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-07 12:48:36 +0100

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,15 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Nav() {
+    //assigning location variable
+    const location = useLocation();
+    console.log(location);
+
     return (
         <nav>
-            <NavLink to="/">Posts</NavLink>
+            <NavLink to="/" className={location.pathname.includes("/posts/") ? "active" : ""}>
+                Posts
+            </NavLink>
             <NavLink to="/create">Create</NavLink>
             <NavLink to="/favs">Favorites</NavLink>
             <NavLink to="/profile">Profile</NavLink>

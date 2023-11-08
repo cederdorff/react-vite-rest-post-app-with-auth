ed5654678ba293b30efd9a5e6dc570d2cf4b8147
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-08 22:46:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: "/react-vite-rest-post-app-with-auth/",
-    plugins: [react()],
-    server: {
-        open: true
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: "/"
+    };
+
+    if (command !== "serve") {
+        config.base = "/react-vite-gh-pages/";
     }
+
+    return config;
 });
